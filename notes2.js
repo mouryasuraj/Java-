b97cb2a738fa5a9265feba5cce65586dfa7107a9
mouryasuraj <mouryasuraj6266444@gmail.com>
2024-07-04 19:19:06 +0530

    changes done in java

---
+++ notes2.js
@@ -0,0 +1,37 @@
+/*
+
+OOPS(Object Oriented Programming System):
+    1. It is a concept which is used in programming languages.
+    2. It is a way or methodoloy to write the code.
+Class and Objects:
+    1. Class is a logical entity.
+    2. Class have properties(data member) and behaviour(methods)
+    3. Creating object with new keyword so that we can use class properties and methods.
+Constructor:
+    1. Constructor is used to initialize the data of the object.
+    2. It will call automatically when we create an object or when we use new keyword.
+    3. Properties of constructor:
+        a. constructor name === className
+        b. constructor does not return any value. not even void
+        c. Public Student(){
+            // This is non-parameterized constructor
+        }.
+        d. Public Student(int a, int b, String name){
+            // This is parameterized constructor
+        }.
+        e. When java creates constructor automatically then only it is called as default constructor. If we don't have any constructor in our class then java will automatically create default one. 
+
+constructor overloading:
+    1. When we have more than one contructor in single class, it is called as constructor overloading.
+    2. We can't create same construtor in a single class we have to add parameter to the constructor.
+    3. We have to give number of different argument for creating objects. So, when number of arguments and data type match then the specified constructor will be called.
+    4. Atlest one must be there for construtor overloading.
+        a. Number of different argument.
+        b. Type of different argument.
+        c. Order of arguments may differ.
+        d. 
+
+Method overloading:
+    1. When we have multiple methods with a same name in a same class with different argument is called as Method overloading.
+    2. 
+*/
